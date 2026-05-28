ad9680613d47883b58c61815343b8ebc9ff185e3
Ivan Goren <ivan.goren@gmail.com>
2026-05-27 22:06:50 -0300

    GJ0005 intro screen

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+  public void OnStartClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    
+  public void OnExitClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying=false;
+        #endif
+
+        Application.Quit();
+    }
+
+    
+  public void OnCreditsClick()
+    {
+    }
+}
