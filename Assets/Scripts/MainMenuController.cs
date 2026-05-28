b40e018081f957717068a7fe2ceac080176c5089
Ivan Goren <ivan.goren@gmail.com>
2026-05-27 22:44:04 -0300

    GJ0006 (perdi la numeracion) pantalla intro maketa

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,23 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-  public void OnStartClick()
+
+    public GameObject mainPageCambas;
+    public GameObject creditsCambas;
+    public void OnStartClick()
     {
         SceneManager.LoadScene("GameScene");
     }
 
-    
-  public void OnExitClick()
+
+    public void OnExitClick()
     {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying=false;
-        #endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
 
         Application.Quit();
     }
 
-    
-  public void OnCreditsClick()
+
+    public void OnCreditsClick()
     {
+        mainPageCambas.SetActive(false);
+        creditsCambas.SetActive(true);
+    }
+
+    public void OnCreditsExitClick()
+    {
+        creditsCambas.SetActive(false);
+        mainPageCambas.SetActive(true);
     }
 }
